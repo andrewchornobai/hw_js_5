386a366e104b47a4ee89c8d856ba4f50e31762fd
ggaspar22 <andrijchornobajj@gmail.com>
2021-12-06 10:02:49 +0200

    added hw 5

---
+++ script.js
@@ -0,0 +1,66 @@
+// 1 zavdannia
+
+// let a = +prompt('write number 1-12');
+// switch(a){
+//     case 1: alert("Зима");
+//         break;
+//     case 2: alert("Зима");
+//         break;
+//     case 3: alert("Весна");
+//         break;
+//     case 4: alert("Весна");
+//         break;
+//     case 5: alert("Весна");
+//         break;
+//     case 6: alert("Літо");
+//         break;
+//     case 7: alert("Літо");
+//         break;
+//     case 8: alert("Літо");
+//         break;
+//     case 9: alert("Осінь");
+//         break;
+//     case 10: alert("Осінь");
+//         break;
+//     case 11: alert("Осінь");
+//         break;
+//     case 12: alert("Зима");
+//         break;
+//     default : alert("Неможливо");
+//         break;
+// }
+
+// 2 zavdannia
+
+// let a = +prompt('write some number');
+// function declaration(a){
+//     if(a%2!=0){
+//        console.log('Це число є простим');
+//     }else if(a%2==0){
+//         console.log('Це число не є простим');
+//     }else if(a==2){
+//         console.log('Це число є простим');
+//     }else if(a==0){
+//         console.log('Ви ввели число 0');
+//     }else if(a<0){
+//         console.log("Ви ввели від'ємні числа");
+//     }else{
+//         console.log('Ви ввели не число');
+//     }
+// }
+// declaration(a);
+
+// 3 zavdannia
+
+// function declaration(a,b,c){
+//     if(a>b&&a>c){
+//         console.log(a);
+//     }else if(b>a&&b>c){
+//         console.log(b);
+//     }else if(c>a&&c>b){
+//     console.log(c);
+//     }
+// }
+
+// declaration(13,21,19);
+
